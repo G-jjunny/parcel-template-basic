66aa3e3c396d5b699203cec42b33bf0624a77107
G-jjunny <rudwns9551@naver.com>
2022-07-25 17:19:58 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+//ESM
+//CommonJS
+
+//import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+// export {
+//     Plugins : [
+//         autoprefixer
+//     ]
+// }
+module.exports = {
+    Plugins : [
+        autoprefixer
+    ]
+}
